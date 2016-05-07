917234605ce02c31f1ab56dc673daeffde7530de
Supason Kotanut <supasonk@gmail.com>
2016-05-07 12:51:59 +0700

    can use axios to pull the course data

--- app/containers/LoginContainer.js
+++ app/containers/LoginContainer.js
@@ -1,8 +1,9 @@
 var React = require('react');
 var styles = require('../styles');
-
+var getAllCourseData = require('../utils/courseHelper').getAllCourseData;
 
 function LoginContainer(props){
+    getAllCourseData();
     return(
         <div className = 'jumbotron col-sm-12 text-center' style ={styles.transparentBg}>
             {props.children}
--- app/utils/courseHelper.js
+++ app/utils/courseHelper.js
@@ -1,1 +1,12 @@
 var axios = require('axios');
+
+function getAllCourseData(){
+    return axios.get('https://whsatku.github.io/skecourses/combined.json')
+    .then(function (courseData) {
+      console.log(courseData)
+      return courseData;
+    });
+}
+module.exports = {
+    getAllCourseData : getAllCourseData
+}
